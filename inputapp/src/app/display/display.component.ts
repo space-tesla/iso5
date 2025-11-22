9fc5cdad7687e6b0cbff5c9bb30b79f4415bc8c9
wisdm-nikhil <nikhil.mhaske@gmail.com>
2025-11-22 19:46:53 +0530

    App Upload Done

---
+++ inputapp/src/app/display/display.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-display',
+  templateUrl: './display.component.html',
+  styleUrls: ['./display.component.css']
+})
+export class DisplayComponent {
+  name: string = "";
+  output: string = "";
+
+  show() {
+    this.output = this.name;
+  }
+}
